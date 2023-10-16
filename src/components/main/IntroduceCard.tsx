d47d4843a98031717e73802f62934ad1f3c12abe
YongYong21 <imkinggosu@gmail.com>
2023-10-16 19:04:34 +0900

    Update IntroduceCard.tsx

--- src/components/main/IntroduceCard.tsx
+++ src/components/main/IntroduceCard.tsx
@@ -1,10 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { useEffect, useState } from "react";
+import IntroduceCardImg from "../../asset/img/IntroduceCardImg.webp";
 
 const introduceContainer = css`
   height: 360vh;
-  background-color: rgba(0, 0, 0);
+
+  background-color: rgba(0, 0, 0, 0.6);
+  background-image: url(${IntroduceCardImg});
+  background-size: 100vw;
+  background-repeat: no-repeat;
+  background-blend-mode: multiply;
+  background-attachment: fixed;
 `;
 const cardBox = (opacityY: number = 1) => css`
   height: 100vh;
@@ -19,15 +26,30 @@ const cardBox = (opacityY: number = 1) => css`
   transition: background-color 0.2s;
 `;
 
+const scrollDownContainer = (opacityScrollDown: number) => css`
+  position: absolute;
+  left: 45%;
+  bottom: 5%;
+
+  font-size: 36px;
+  color: white;
+  opacity: ${opacityScrollDown};
+  transition: opacity 0.5s;
+`;
 function IntroduceCard(): JSX.Element {
   const [cardBoxState, setCardBoxState] = useState([
     { opacity: 1 },
     { opacity: 1 },
     { opacity: 1 }
   ]);
-
+  const [opacityScrollDown, setOpcaityScrollDown] = useState(0.5);
   const handleScroll = () => {
     const scrollY = window.scrollY;
+    if (scrollY > 10) {
+      setOpcaityScrollDown(0);
+    } else {
+      setOpcaityScrollDown(0.5);
+    }
 
     if (scrollY < window.innerHeight) {
       const opacity = (-1 / window.innerHeight) * scrollY + 1;
@@ -62,6 +84,9 @@ function IntroduceCard(): JSX.Element {
 
   return (
     <>
+      <div css={scrollDownContainer(opacityScrollDown)}>
+        <span>Scroll Down</span>
+      </div>
       <div css={introduceContainer}>
         <div css={cardBox(cardBoxState[0].opacity)}>안녕하세요</div>
         <div css={cardBox(cardBoxState[1].opacity)}>
