6dc2e25765ee77ba7282dff7f85d18065ce3b55b
YongYong21 <imkinggosu@gmail.com>
2023-10-30 03:46:09 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import IntroduceCard from "./components/main/IntroduceCard";
+import ProfileCard from "./components/main/profileCard";
+import ProjectConponents from "./components/main/ProjectComponents";
+import EmailComponents from "./components/main/EmailComponents";
+
+const AppContainer = css`
+  height: auto;
+`;
 
 function App(): JSX.Element {
   return (
-    <>
+    <div css={AppContainer}>
       <IntroduceCard></IntroduceCard>
-    </>
+      <ProfileCard></ProfileCard>
+      <ProjectConponents></ProjectConponents>
+      <EmailComponents></EmailComponents>
+    </div>
   );
 }
 
